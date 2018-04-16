aba75bcf5780019cd1da7d44be5ad88185cda64e
Jeroen Vervaeke <jeroen@vervaeke.nu>
2018-04-17 00:00:33 +0200

    Initial commit

---
+++ examples/counter/src/main.rs
@@ -0,0 +1,24 @@
+extern crate redux_rs;
+
+use redux_rs::*;
+
+fn main() {
+    let mut store = Store::new(test_reducer, 0);
+    store.subscribe(|state| println!("{}", state));
+    store.dispatch(CounterActions::INCREMENT(1));
+    store.dispatch(CounterActions::DECREMENT(2));
+    store.dispatch(CounterActions::INCREMENT(3));
+
+}
+
+enum CounterActions {
+    INCREMENT(i32),
+    DECREMENT(i32)
+}
+
+fn test_reducer(state: &i32, action: CounterActions) -> i32 {
+    match action {
+        CounterActions::INCREMENT(x) => state + x,
+        CounterActions::DECREMENT(x) => state - x,
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,102 @@
+pub type Reducer<TState, TAction> = fn(&TState, TAction) -> TState;
+pub type Subscriber<TState> = fn(&TState);
+
+pub struct Store<TState, TAction> {
+    reducer: Reducer<TState, TAction>,
+    state: TState,
+    subscribers: Vec<Subscriber<TState>>,
+}
+
+impl<TState, TAction> Store<TState, TAction> {
+    pub fn new(reducer: Reducer<TState, TAction>, default_state: TState) -> Store<TState, TAction> {
+        Store::<TState, TAction> {
+            reducer,
+            state: default_state,
+            subscribers: Vec::new(),
+        }
+    }
+
+    pub fn dispatch(&mut self, action: TAction) {
+        self.state = (self.reducer)(&self.state, action);
+        self.subscribers.iter().for_each(|subscriber| subscriber(&self.state));
+    }
+
+    pub fn subscribe(&mut self, subscriber: Subscriber<TState>) {
+        self.subscribers.push(subscriber);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    enum TestActions {
+        INCREMENT(i32),
+        DECREMENT(i32),
+    }
+
+    fn test_reducer(state: &i32, action: TestActions) -> i32 {
+        match action {
+            TestActions::INCREMENT(x) => state + x,
+            TestActions::DECREMENT(x) => state - x,
+        }
+    }
+
+    #[test]
+    fn new_default_state_works() {
+        // Create the store
+        let store = Store::<i32, TestActions>::new(test_reducer, 1);
+
+        // Test if the default state is set as desired
+        let expected = 1;
+        let actual = store.state;
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn dispatch_increment_once() {
+        // Create the store
+        let mut store = Store::<i32, TestActions>::new(test_reducer, 1);
+
+        // Dispatch our first action
+        store.dispatch(TestActions::INCREMENT(1));
+
+        // Test if the new state has the desired value
+        let expected = 2;
+        let actual = store.state;
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn dispatch_decrement_once() {
+        // Create the store
+        let mut store = Store::<i32, TestActions>::new(test_reducer, 1);
+
+        // Dispatch our first action
+        store.dispatch(TestActions::DECREMENT(1));
+
+        // Test if the new state has the desired value
+        let expected = 0;
+        let actual = store.state;
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn dispatch_mixed() {
+        // Create the store
+        let mut store = Store::<i32, TestActions>::new(test_reducer, 0);
+
+        // Dispatch multiple actions
+        store.dispatch(TestActions::DECREMENT(12));
+        store.dispatch(TestActions::DECREMENT(31));
+        store.dispatch(TestActions::INCREMENT(15));
+        store.dispatch(TestActions::DECREMENT(78));
+        store.dispatch(TestActions::INCREMENT(12));
+        store.dispatch(TestActions::INCREMENT(14));
+
+        // Test if the new state has the desired value
+        let expected = -80;
+        let actual = store.state;
+        assert_eq!(expected, actual)
+    }
+}
